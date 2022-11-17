74c9531aa2d53e89362b116ca88bc7b03c9330c7
Ibraheem Rodrigues <28712361+IbraheemR@users.noreply.github.com>
2022-11-17 17:24:35 +0000

    ExB, get encoder working for ExC

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Encoder.h>
 
 #define TRIMMER_PIN A0
 
@@ -9,42 +10,104 @@ void setupA()
 {
   Serial.println("Running A");
 }
-void setupB()
-{
-  Serial.println("Running B");
-}
-void setupC()
-{
-  Serial.println("Running C");
-}
-void setupD()
-{
-  Serial.println("Running D");
-}
-void setupE()
-{
-  Serial.println("Running E");
-}
 
 void loopA()
 {
   int val = analogRead(TRIMMER_PIN);
   float voltage = val / 1023.0 * 5.0;
-  Serial.print(val);
-  Serial.print("=");
+  Serial.print(val / 1023.0 * 100.0);
+  Serial.print("% = ");
   Serial.print(voltage);
   Serial.println("V");
   delay(100);
 }
 
+// -----------------
+
+#define MOTOR_A 4
+#define MOTOR_B 5
+
+void setupB()
+{
+  pinMode(MOTOR_A, OUTPUT);
+  pinMode(MOTOR_B, OUTPUT);
+  Serial.println("Press F for FORWARD, B for BACKWARDS or H for HALT");
+}
+
 void loopB()
 {
-  Serial.print("B");
+  if (Serial.available()) {
+    char c = Serial.read();
+    c = toupper(c);
+    switch (c)
+    {
+    case 'F':
+      digitalWrite(MOTOR_A, HIGH);
+      digitalWrite(MOTOR_B, LOW);
+      Serial.println("FORWARDS");
+      break;
+
+    case 'B':
+      digitalWrite(MOTOR_A, LOW);
+      digitalWrite(MOTOR_B, HIGH);
+      Serial.println("BACKWARDS");
+      break;
+
+    case 'H':
+      digitalWrite(MOTOR_A, LOW);
+      digitalWrite(MOTOR_B, LOW);
+      Serial.println("HALT");
+      break;
+    
+    default:
+      break;
+    }
+  }
+}
+
+// -----------------
+
+#define MOTOR_SENSE_A 2
+#define MOTOR_SENSE_B 3
+
+Encoder encoder(MOTOR_SENSE_A, MOTOR_SENSE_B);
+
+unsigned long lastTime = 0;
+int32_t lastPosition = 0;
+
+void setupC()
+{  
+  setupB();
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loopC()
 {
-  Serial.print("C");
+  loopB();
+
+  int32_t position = encoder.read();
+  unsigned long now = millis();
+
+  unsigned long timeDiff = now - lastTime;
+  if (timeDiff >= 1000) {
+    int32_t posDiff = position - lastPosition;
+    int32_t rotations = posDiff / (12.0 * 50.0 * 4.5);
+    float speed = - rotations / (timeDiff / 1000.0 / 60.0);
+
+    lastPosition = position;
+    lastTime = now;
+
+    Serial.print(speed);
+    Serial.println("rpm");
+  }
+}
+
+// -----------------
+
+void setupD()
+{
+  Serial.println("Running D");
 }
 
 void loopD()
@@ -52,10 +115,19 @@ void loopD()
   Serial.print("D");
 }
 
+// -----------------
+
+void setupE()
+{
+  Serial.println("Running E");
+}
+
 void loopE()
 {
   Serial.print("E");
 }
+
+// ---------------------------------
 
 void setup()
 {
@@ -99,27 +171,31 @@ void loop()
 
   if (mode == 'A')
   {
+    Serial.println("Progam A:");
     setupA();
     hasMode = true;
   }
   else if (mode == 'B')
   {
+    Serial.println("Progam B:");
     setupB();
     hasMode = true;
   }
   else if (mode == 'C')
   {
+    Serial.println("Progam C:");
     setupC();
     hasMode = true;
-
   }
   else if (mode == 'D')
   {
+    Serial.println("Progam D:");
     setupD();
     hasMode = true;
   }
   else if (mode == 'E')
   {
+    Serial.println("Progam E:");
     setupE();
     hasMode = true;
   }
